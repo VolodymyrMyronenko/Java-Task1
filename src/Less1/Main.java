4fe3bd9304c2cd8c5d858f6b12513f5c81b5cfff
Volodymyr Myronenko <volodmyr2911@gmail.com>
2019-11-04 16:50:44 +0200

    Initial commit

---
+++ src/Less1/Main.java
@@ -0,0 +1,56 @@
+package Less1;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    private static int failsCount = 0;
+
+    public static void main(String[] args) {
+        char[][] words = new char[][]{{'t', 'h', 'i', 's'}, {'i', 's'}, {'a'}, {'w', 'o', 'r', 'd'}};
+
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words[i].length; j++) {
+                System.out.print(words[i][j]);
+            }
+            System.out.println();
+        }
+
+        Random random = new Random();
+        int secretWordIndex = random.nextInt(words.length);
+        System.out.println(words[secretWordIndex]);
+        int secretWordLength = words[secretWordIndex].length;
+        char[] secretWord = new char[secretWordLength];
+        Arrays.fill(secretWord, '*');
+        final char[] openWord = words[secretWordIndex];
+//        System.out.println(secretWord);
+        System.out.println(secretWord);
+     do {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введіть літеру: ");
+        String letter = in.nextLine();
+
+        System.out.println("Введіть позицію літери");
+        int position = in.nextInt();
+
+        System.out.printf("Letter: %s index: %d", letter, position);
+
+        char userLetter = letter.toCharArray()[0];
+
+        if (Objects.equals(userLetter, openWord[position])) {
+            secretWord[position] = userLetter;
+        } else {
+            failsCount++;
+        }
+        System.out.println();
+        System.out.println(secretWord);
+        System.out.println("Кількість невдалих спроб: " + failsCount);
+       }while (secretWord != openWord);
+
+
+    }
+}
